b19587d8b6b3c089b95af64923142802840ea3a6
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2025-03-13 12:56:04 +0100

    Progress on the dashbord, forgot to commit early

--- script.js
+++ script.js
@@ -2,9 +2,41 @@ document.addEventListener("DOMContentLoaded", function () {
   //Function for the clock in headerr----------------------------
   function clockUpdate() {
     const now = new Date();
-    const timeString = now.toLocaleTimeString();
-    document.getElementById("clock").textContent = timeString;
+    /*Time variables*/
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const hours = now.getHours().toString().padStart(2, "0");
+    const timeString = `${hours}:${minutes}`;
+    /*Date variables*/
+    const day = now.getDay().toString().padStart(2, "0");
+    const month = now.toLocaleString("sv-SE", { month: "long" });
+    const year = now.getFullYear();
+    const dateString = `${day} ${month} ${year}`;
+    /*Combined variables to represent time & date*/
+    const clockElement = `${timeString}   ${dateString}`;
+    /*Seperated time & date to span to easily adjust the css*/
+    document.getElementById("clock").innerHTML = `
+    <span class="time">${timeString}</span>
+    <span class="date">${dateString}</span>`;
   }
   setInterval(clockUpdate, 1000);
   clockUpdate();
+
+  const titleElement = document.getElementById("title__edit");
+  /*Added so enter doesnt make new rows only accepts title*/
+  titleElement.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      titleElement.blur();
+    }
+  });
+
+  function checkTitle() {
+    if (titleElement.textContent.trim() === "") {
+      titleElement.classList.add("empty");
+    } else {
+      titleElement.classList.remove("empty");
+    }
+  }
+  checkTitle();
+  titleElement.addEventListener("input", checkTitle);
 });
